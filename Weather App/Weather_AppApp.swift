1ea0a9527491b8cd7eddbe835349c6accd69f402
Jc Castano <jccastano86@gmail.com>
2024-05-07 19:57:46 -0400

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Jc Castano on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
